17dd1370b5d01d15ec29c852b9e6f86a52dd05c9
Benjamin Lazarecki <benjamin.lazarecki@gmail.com>
2012-12-07 00:00:16 +0100

    [Registration] Add username and captcha

--- config/autoload/zfcuser.global.php
+++ config/autoload/zfcuser.global.php
@@ -41,7 +41,7 @@ $settings = array(
      *
      * Accepted values: boolean true or false
      */
-    //'enable_username' => false,
+    'enable_username' => true,
 
     /**
      * Enable Display Name
@@ -100,7 +100,7 @@ $settings = array(
      * Determines if a captcha should be utilized on the user registration form.
      * Default value is false.
      */
-    //'use_registration_form_captcha' => false,
+    'use_registration_form_captcha' => true,
 
     /**
      * Form Captcha Options
